f3912b9d16da11d40ba4149b742d53898ce9d54a
Kishan Kumar <kishankrs@outlook.com>
2022-09-21 15:43:34 +0530

    user input + fix

--- deploy.py
+++ deploy.py
@@ -13,33 +13,43 @@ def home():
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    radius_mean = request['radius_mean']
-    texture_mean = request['texture_mean']
-    perimeter_mean = request['perimeter_mean']
-    area_mean = request['area_mean']
-    compactness_mean = request['compactness_mean']
-    concavity_mean = request['concavity_mean']
+    radius_mean = float(request['radius_mean'])
+    texture_mean = float(request['texture_mean'])
+    perimeter_mean = float(request['perimeter_mean'])
+    area_mean = float(request['area_mean'])
+    compactness_mean = float(request['compactness_mean'])
+    concavity_mean = float(request['concavity_mean'])
     # possibility of error in concave points_mean
-    concave_points_mean = request['concave points_mean']
-    symmetry_mean = request['symmetry_mean']
-    radius_se = request['radius_se']
-    perimeter_se = request['perimeter_se']
-    area_se = request['area_se']
-    smoothness_se = request['smoothness_se']
-    compactness_se = request['compactness_se']
+    concave_points_mean = float(request['concave points_mean'])
+    symmetry_mean = float(request['symmetry_mean'])
+    radius_se = float(request['radius_se'])
+    perimeter_se = float(request['perimeter_se'])
+    area_se = float(request['area_se'])
+    smoothness_se = float(request['smoothness_se'])
+    compactness_se = float(request['compactness_se'])
     # possibility of error in concave points_se
-    concave_points_se = request['concave points_se']
-    symmetry_se = request['symmetry_se']
-    fractal_dimension_se = request['fractal_dimension_se']
-    radius_worst = request['radius_worst']
-    texture_worst = request['texture_worst']
-    perimeter_worst = request['perimeter_worst']
-    area_worst = request['area_worst']
-    smoothness_worst = request['smoothness_worst']
-    concavity_worst = request['concavity_worst']
+    concave_points_se = float(request['concave points_se'])
+    symmetry_se = float(request['symmetry_se'])
+    fractal_dimension_se = float(request['fractal_dimension_se'])
+    radius_worst = float(request['radius_worst'])
+    texture_worst = float(request['texture_worst'])
+    perimeter_worst = float(request['perimeter_worst'])
+    area_worst = float(request['area_worst'])
+    smoothness_worst = float(request['smoothness_worst'])
+    concavity_worst = float(request['concavity_worst'])
     # possibility of error in concave points_worst
-    concave_points_worst = request['concave points_worst']
-    symmetry_worst = request['symmetry_worst']
+    concave_points_worst = float(request['concave points_worst'])
+    symmetry_worst = float(request['symmetry_worst'])
+
+    result = model.predict([[radius_mean, texture_mean, perimeter_mean, area_mean,
+                             compactness_mean, concavity_mean, concave_points_mean,
+                             symmetry_mean, radius_se, perimeter_se, area_se,
+                             smoothness_se, compactness_se, concave_points_se, symmetry_se,
+                             fractal_dimension_se, radius_worst, texture_worst,
+                             perimeter_worst, area_worst, smoothness_worst, concavity_worst,
+                             concave_points_worst, symmetry_worst]])[0]
+
+    return render_template('index.html', **locals)
 
 
 if __name__ == '__main__':
